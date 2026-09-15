c69d2697ff23bd9421bbb77ac92720cd4e684c61
msk_reddy_chirla <mskreddy9963@gmail.com>
2026-09-15 12:00:00 +0530

    added basename to the browser router

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/simple-ecommerce-dashboard">
         <HashRouter>
             <App />
         </HashRouter>
